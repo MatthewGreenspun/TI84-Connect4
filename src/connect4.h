ff6dc4057ce09fb7b437b788312121a43a389a49
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2021-10-14 22:09:42 -0400

    added addPiece function

--- src/connect4.h
+++ src/connect4.h
@@ -3,7 +3,7 @@
 
 #define BOARD_WIDTH 300
 #define BOARD_HEIGHT 210
-#define BOARD_OFFSET 5
+#define BOARD_OFFSET 10
 #define WHITE 255
 #define RED 224
 #define YELLOW 231
@@ -15,15 +15,15 @@
 typedef enum circleState_t {EMPTY = WHITE, RED_PIECE = RED, YELLOW_PIECE = YELLOW, VISITED} circleState_t;
 
 typedef struct board_t {
-	circleState_t gameState[6][7];
+	circleState_t boardState[6][7];
 	uint8_t selectedCol;
+	uint8_t colHeights[7];
 } board_t;
 
 void boardInit(board_t* b);
 void drawBoardBackground(void);
 void drawPieces(board_t* b);
 void drawSelectedCol(board_t* b);
-
-
+bool addPiece(board_t* b, uint8_t col, circleState_t color);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -5,18 +5,23 @@
 
 void boardInit(board_t* b){
 	b->selectedCol = 3;
+
+	int i;
+	for(i = 0; i < 7; i++)
+		b->colHeights[i] = 0;
+
 	uint8_t row;
 	for(row = 0; row < 6; ++row){
 		uint8_t col;
 		for(col = 0; col < 7; ++col){
-			b->gameState[row][col] = EMPTY;
+			b->boardState[row][col] = EMPTY;
 		}
 	}
 }
 
 void drawBoardBackground(void){
 	gfx_SetColor(BOARD_BG_CLR);
-	gfx_FillRectangle_NoClip(0, 0, BOARD_WIDTH, BOARD_HEIGHT);
+	gfx_FillRectangle_NoClip(BOARD_OFFSET, 0, BOARD_WIDTH, BOARD_HEIGHT);
 }
 
 void drawPieces(board_t* b){
@@ -24,8 +29,8 @@ void drawPieces(board_t* b){
 	for(row = 0; row < 6; ++row){
 		uint8_t col;
 		for(col = 0; col < 7; ++col){
-			gfx_SetColor(b->gameState[row][col]);
-			uint16_t x = (BOARD_WIDTH/7)*col + BOARD_WIDTH/14;
+			gfx_SetColor(b->boardState[row][col]);
+			uint16_t x = (BOARD_WIDTH/7)*col + BOARD_WIDTH/14 + BOARD_OFFSET;
 			uint16_t y = (BOARD_HEIGHT/6)*row + BOARD_WIDTH/14;
 			gfx_FillCircle_NoClip(x, y, 10);
 		}
@@ -34,11 +39,23 @@ void drawPieces(board_t* b){
 
 void drawSelectedCol(board_t* b){
 	gfx_SetColor(SELECTED_COL_CLR);
-	gfx_FillRectangle_NoClip(b->selectedCol*(BOARD_WIDTH/7) + 5, 0, BOARD_HEIGHT/7, 210);
+	gfx_FillRectangle_NoClip(b->selectedCol*(BOARD_WIDTH/7) + 5 + BOARD_OFFSET, 0, BOARD_HEIGHT/7, 210);
+}
+
+bool addPiece(board_t* b, uint8_t col, circleState_t color){
+	if(b->colHeights[col] == 6) 
+		return false;
+
+	uint8_t row = 5 - b->colHeights[col];
+	b->boardState[row][col] = color;
+	b->colHeights[col]++;
+	return true;
 }
 
 int main(void){
 	board_t board;
+	bool isRedsTurn = true;
+
 	boardInit(&board);
 
 	//screen is 320 wide x 240 high
@@ -56,8 +73,7 @@ int main(void){
 		// Load group 7 registers
 		key = kb_Data[7];
 
-		switch (key)
-		{
+		switch (key){
 				case kb_Right:     
 						board.selectedCol = (board.selectedCol + 1) % 7;
 						drawBoardBackground();
@@ -75,10 +91,23 @@ int main(void){
 				default:
 						break;
 		}
+
+		kb_Scan();
+		if(kb_Data[6] == kb_Enter){
+			bool moved = addPiece(&board, board.selectedCol, isRedsTurn? RED_PIECE : YELLOW_PIECE);
+			drawBoardBackground();
+			drawSelectedCol(&board);
+			drawPieces(&board);
+			if(moved && isRedsTurn)
+				isRedsTurn = false;
+			else if(moved)
+				isRedsTurn = true;
+			delay(500);
+		}
     } while (kb_Data[1] != kb_2nd);
 
-
 	while(!os_GetCSC());
+
 	gfx_End();
 
 	return 0;
