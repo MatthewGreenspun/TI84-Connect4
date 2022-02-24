227910f8764d062afd884294fced9b83b3abccec
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2022-02-24 11:11:53 -0500

    edit build script for debug

--- buildrun.sh
+++ buildrun.sh
@@ -1,2 +1,10 @@
-../../bin/make.exe
-CEmu -s ../../bin/clibs.8xg -s bin/CONNECT4.8xp --launch CONNECT4 
+../../bin/make.exe clean
+if [ $1 = "debug" ]; then
+	echo "building for debug ..."
+	../../bin/make.exe debug
+	CEmu -s ../../bin/clibs.8xg -s bin/CONNECT4.8xp --launch CONNECT4 
+else 
+	echo "building for production ..."
+	../../bin/make.exe
+	CEmu -s ../../bin/clibs.8xg -s bin/CONNECT4.8xp --launch CONNECT4 
+fi
